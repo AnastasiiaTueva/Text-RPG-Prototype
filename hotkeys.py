8684235b0b82f569de2e1a7008ee572b9abfeafe
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-04-06 20:09:20 +0300

    Add files via upload
    A function for the menu and hotkeys has been added for future inventory and viewing of stats.

---
+++ hotkeys.py
@@ -0,0 +1,12 @@
+import Events
+
+
+def ui():
+    width = 60
+
+
+    print("\n " + "-" * width + " ")
+
+    menu = "(I) - Inventory | (S) - Stats | (X) - Exit"
+    print("|" + menu.center(width) + "|")
+    print(" " + "-" * width + " ")
