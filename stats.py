1786dc0d393e61a2a989b05b06b95cf26f0e34da
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-04-07 23:45:07 +0300

    Update stats.py
    Relocated the character attribute input function.
    Added direct attribute input to the character class for improved flexibility.

--- stats.py
+++ stats.py
@@ -1,8 +1,18 @@
-def stat():
-    Points = 10
-    Luck = 1
-    Strength = 1
-    Agility = 1
-    Intelligence = 1
+import character
 
-    return Luck, Strength, Agility, Intelligence, Points
+def choose_stats():
+    
+    print("Before starting the game, define your character's attributes.\n You have points that you can use to increase your stats.\n After that, you won't be able to change them.")
+    print("Luck, Strength, Agility, Intelligence\n")
+
+    Luck = int(input("Luck: "))
+    character.Hero.luck += Luck
+
+    Strength = int(input("Strength: "))
+    character.Hero.strength += Strength
+
+    Agility = int(input("Agility: "))
+    character.Hero.agility += Agility
+    
+    Intelligence = int(input("Intelligence: "))
+    character.Hero.intelligence += Intelligence
