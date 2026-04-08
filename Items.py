98ecfa27f4b4a5d1516d5e4eaccf729fbddb761d
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-04-08 20:17:27 +0300

    Update Items.py
    Added items obtainable at the start of the game.

--- Items.py
+++ Items.py
@@ -5,3 +5,11 @@ class Items:
         self.price = price
 
 Bone = Items("a Monster bone. Might come in handy", 10)
+
+Stick = Items("A smooth handle for future axe, pickaxe, or something similar. Doesn't sound very useful, but with enough imagination, it might prove handy.", 20)
+
+Rope = Items(" A three-meter length of rope. It looks almost unused, as if someone placed it here recently. With the right skill, it could be very useful", 15)
+
+Dagger = Items("A small silver dagger. It looks worn, but still usable. Compared to your iron sword, it may not seem like a great option, but it could give you an advantage against enemies.", 15)
+
+Horseshoe = Items("Looks very well crafted. likely made by a renowned blacksmith. It may not seem useful, but if you are superstitious, it might bring you a bit of luck.", 10)
