ce464891bda8690115616570f4c86b025187b43a
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-06-22 22:03:42 +0300

    Update mobs.py
    Added comments to the code

--- mobs.py
+++ mobs.py
@@ -1,5 +1,7 @@
 import random 
 
+
+# Creating a monster class and characteristics
 class Mob:
 
     def __init__(self, health, minDamage,maxDamage, XP):
@@ -12,12 +14,15 @@ class Mob:
         return random.randint(self.minDamage, self.maxDamage)
 
 
+# Creating a Skeleton object
 Skeleton = Mob(20, 1, 4, 50)
 
+
+# Creating a Slime object
 Slime = Mob(15, 1, 2, 25)
 
+# Creating randomization of monster spawns
 def randomMob():
     Mobs = [Skeleton, Slime]
     random_mobs = random.choice(Mobs)
     print(f"\nYou encountered {random_mobs}")
-    return random_mobs
