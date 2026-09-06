3642e09c4b680482b3f657757d5085654b077c65
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-06 18:38:12 +0300

    Update difficulty.py
    Added a new function to track the number of rooms completed.

--- difficulty.py
+++ difficulty.py
@@ -1,20 +1,31 @@
 def difficult():
 
     difficulty_choice = input("Select difficulty(easy, hard, hardcore):").lower()
-
-    easy = 5
-    hard = 10
-    hardcore = 15
+    easy = "easy"
+    hard = "hard"
+    hardcore = "hardcore"
     match difficulty_choice:
 
         case "easy":
-            
             return 1
 
         case "hard":
-
             return 1.5
 
         case "hardcore":
-
             return 2
+
+def difficult_rooms():
+
+    difficulty_choice = input("Select difficulty(easy, hard, hardcore):").lower()
+
+    match difficulty_choice:
+
+        case "easy":
+            return 5
+
+        case "hard":
+            return 10
+
+        case "hardcore":
+            return 15
