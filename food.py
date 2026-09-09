1900789907f49d22308aaaf22c28e91d8a2c6268
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-09 19:44:04 +0300

    Update food.py
    Remove price parameter from Food class and move food object creation outside randomFood()

--- food.py
+++ food.py
@@ -3,23 +3,21 @@ import random
 # Creating a food class and characteristics
 class Food:
 
-    def __init__(self, name, description, heal, price):
+    def __init__(self, name, description, heal):
         self.name = name
         self.description = description
         self.heal = heal
-        self.price = price
     
     def __str__(self):
         return self.name
 
-
 # Creating a food objects
+
+apple = Food("Apple", "A juicy ripe apple", 10),
+bread = Food("Bread", "A simple fresh bread", 12)
 
 def randomFood():
 
-    foods = [
-            Food("Apple", "A juicy ripe apple", 10, 10),
-            Food("Bread", "A simple fresh bread", 12, 15),
-    ]
+    foods = [apple, bread]
     
     return random.choice(foods)
