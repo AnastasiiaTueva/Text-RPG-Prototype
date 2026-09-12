d15593767d69a12db528b7ab7148cb666511ccd3
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-12 14:05:24 +0300

    Add files via upload
    Add file for text animation in the game

---
+++ text.py
@@ -0,0 +1,17 @@
+import sys 
+import time
+import msvcrt
+
+def typetext(text, delay = 0.03):
+    for i, char in enumerate(text):
+        if msvcrt.kbhit():
+            msvcrt.getch()
+            sys.stdout.write(text[i:])
+            sys.stdout.flush()
+            print()
+            return
+
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
