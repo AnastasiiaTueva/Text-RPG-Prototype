9ce9879af15673d155ae67858c1f0bb1c53d6189
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-12 14:08:18 +0300

    Update Events.py
    Fix bug causing merchant event and monster battle to run simultaneously.
    Add animated text with skip functionality.

--- Events.py
+++ Events.py
@@ -7,12 +7,14 @@ import mobs
 import Items
 import Locations
 import food
+import text
 
 def clear():
     os.system('cls')
 
 event = 0
 coins = 0
+merchant = False
 
 def quest1(mobH):
 
@@ -20,19 +22,40 @@ def quest1(mobH):
     global coins
     clear()
 
-    # Create random location
-    Locations.locationGen()
-
     foods = food.randomFood()
 
     # Spawn random Monster
     mob = mobs.randomMob()
-
     mob.health = mob.health * (mobH // 5)
 
-    print(f"You encountered {mob.name}")
+    # Create random location
+    Locations.locationGen()
+    text.typetext(f"You encountered {mob.name}")
+
+
+    if event == 3 and not merchant:
+        clear()
+        text.typetext(f"You encountered a wandering merchant:\n" \
+        f"You have {coins} coins")
+        text.typetext("What do you want to buy?\n" \
+        "Apple - 2 coins\n" \
+        "Bread - 3 coins\n")
+        buying = input("(Apple/Bread/No): ").lower()
+        if buying == "apple" and coins >= 2:
+            hotkeys.inventory.append(food.apple)
+            input("You bought an Apple!")
+            clear()
+        elif buying == "bread" and coins >= 3:
+            hotkeys.inventory.append(food.bread)
+            input("You bought a Bread!")
+            clear()
+        else:
+            input("You entered the command incorrectly. Press any key to continue.")
+            clear()
+
 
     while True:
+
         # Creating the fight and results
         hotkeys.ui()
         do = input("\nAttack or Dodge?(A/D):").lower()
@@ -43,53 +66,39 @@ def quest1(mobH):
                 clear()
 
                 mob.health -= character.Hero.attack()
-                print(f"The {mob.name} has {mob.health} health left")
+                text.typetext(f"The {mob.name} has {mob.health} health left")
 
                 if mob.health <= 0:
                     clear()
-                    print(f"The {mob.name} is defeated. You gained {mob.XP} XP. You got a {foods.name}.")
+                    text.typetext(f"The {mob.name} is defeated. You gained {mob.XP} XP. You got a {foods.name}.")
                     hotkeys.inventory.append(foods)
                     character.Hero.XP += mob.XP
                     event += 1
                     coins += 1
-                    print(f"finished event(s): {event}")
+                    text.typetext(f"finished event(s): {event}")
                     input("Press any key to continue...")
                     clear()
                     break
 
                 character.Hero.health -= mob.attack()
-                print(f"The {mob.name} attacked you. You have {character.Hero.health} health left")
+                text.typetext(f"The {mob.name} attacked you. You have {character.Hero.health} health left")
       
                 if character.Hero.health <=0:
                     
-                    print("You have been defeated")
+                    text.typetext("You have been defeated")
                     input("Press any key to continue...")
                     clear()
                     exit()
 
-                if event == 3:
-                    print(f"You encountered a wandering merchant:\n" \
-                    f"You have {coins} coins")
-                    print("What do you want to buy?\n" \
-                    "Apple - 2 coins\n" \
-                    "Bread - 3 coins\n")
-                    buying = input("(Apple/Bread/No): ").lower()
-                    if buying == "apple" and coins >= 2:
-                        hotkeys.inventory.append(food.apple)
-                    elif buying == "bread" and coins >= 3:
-                        hotkeys.inventory.append(food.bread)
-                    else:
-                        input("You entered the command incorrectly. Press any key to continue.")
-                        clear()
-                    
+               
         elif do == "d":
 
             clear()
             if random.random() < 0.2 * character.Hero.agility:
-                print(f"The {mob.name} attacked you, but you dodged.")
+                text.typetext(f"The {mob.name} attacked you, but you dodged.")
             else:
                 character.Hero.health -= mob.attack()
-                print(f"You tried to dodge, but failed. You have {character.Hero.health} health left.")
+                text.typetext(f"You tried to dodge, but failed. You have {character.Hero.health} health left.")
         
         elif do == "i":
             hotkeys.I()
