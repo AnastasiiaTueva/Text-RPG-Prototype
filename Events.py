f989820f6e65229ce93f4af58cf92cab30b2de58
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-13 13:36:55 +0300

    Update Events.py
    Add three player levels

--- Events.py
+++ Events.py
@@ -51,6 +51,8 @@ def quest1(mobH, Rtype):
             input("You entered the command incorrectly. Press any key to continue.")
             clear()
 
+        
+
     while True:
 
         # Creating the fight and results
@@ -73,6 +75,19 @@ def quest1(mobH, Rtype):
                     event += 1
                     coins += 1
                     text.typetext(f"finished event(s): {event}")
+                    if character.Hero.XP == 100:
+                        character.Hero.level += 1
+                        character.Hero.health += 5
+                        text.typetext(f"You gained {character.Hero.level} level!")
+                    elif character.Hero.XP == 200:
+                        character.Hero.level += 1
+                        character.Hero.health += 5
+                        text.typetext(f"You gained {character.Hero.level} level!")
+                    elif character.Hero.XP == 300:
+                        character.Hero.level += 1
+                        character.Hero.health +=5
+                        text.typetext(f"You gained {character.Hero.level} level!")
+                        
                     input("Press any key to continue...")
                     clear()
                     break
