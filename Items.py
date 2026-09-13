8fec18b093f79b4f2f976d16e88c06fb9c023168
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-13 13:00:52 +0300

    Update Items.py
    Bug fix

--- Items.py
+++ Items.py
@@ -1,17 +1,26 @@
+import random
+
 # Creating an item class and characteristics
 class Items:
 
-    def __init__(self, description, price):
+    def __init__(self, name, description, price):
+        self.name = name
         self.description = description
         self.price = price
 
 # Creating an item objects
-Bone = Items("a Monster bone. Might come in handy", 10)
+Bone = Items("Bone", "a Monster bone. Might come in handy", 10)
 
-Stick = Items("A smooth handle for future axe, pickaxe, or something similar. Doesn't sound very useful, but with enough imagination, it might prove handy.", 20)
+Stick = Items("Stick", "A smooth handle for future axe, pickaxe, or something similar. Doesn't sound very useful, but with enough imagination, it might prove handy.", 20)
 
-Rope = Items(" A three-meter length of rope. It looks almost unused, as if someone placed it here recently. With the right skill, it could be very useful", 15)
+Rope = Items("Rope", " A three-meter length of rope. It looks almost unused, as if someone placed it here recently. With the right skill, it could be very useful", 15)
 
-Dagger = Items("A small silver dagger. It looks worn, but still usable. Compared to your iron sword, it may not seem like a great option, but it could give you an advantage against enemies.", 15)
+Dagger = Items("Dagger", "A small silver dagger. It looks worn, but still usable. Compared to your iron sword, it may not seem like a great option, but it could give you an advantage against enemies.", 15)
 
-Horseshoe = Items("Looks very well crafted. likely made by a renowned blacksmith. It may not seem useful, but if you are superstitious, it might bring you a bit of luck.", 10)
+Horseshoe = Items("Horseshoe", "Looks very well crafted. likely made by a renowned blacksmith. It may not seem useful, but if you are superstitious, it might bring you a bit of luck.", 10)
+
+def randomItem():
+
+    item = [Bone, Stick, Rope, Dagger, Horseshoe]
+    
+    return random.choice(item)
