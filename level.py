daf5393c58742466cdfea602bcda4118e259e361
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-15 19:38:07 +0300

    Update level.py
    Add code comments

--- level.py
+++ level.py
@@ -1,16 +1,23 @@
 import character
 import text
 
+#Function for character levels
 def leveling():
+
     if character.Hero.XP == 100:
+
         character.Hero.level += 1
         character.Hero.health += 5
         text.typetext(f"You gained {character.Hero.level} level!")
+        
     elif character.Hero.XP == 200:
+
         character.Hero.level += 1
         character.Hero.health += 5
         text.typetext(f"You gained {character.Hero.level} level!")
+
     elif character.Hero.XP == 300:
+
         character.Hero.level += 1
         character.Hero.health +=5
         text.typetext(f"You gained {character.Hero.level} level!")
