0449bdddebdb33348eb4d3a21c696e881e41fb6c
AnastasiiaTueva <nastya.tueva32@gmail.com>
2026-09-15 19:32:42 +0300

    Update main.py
    Add code comments

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def clear():
 
 clear()
 
+#Function for selecting the game difficulty
 rooms = difficulty.difficult()
 
+#Variable for counting the number of completed rooms.
 Events.event = 0
 
 clear()
@@ -25,32 +27,39 @@ stats.beginning()
 
 clear()
 
+#Event function for obtaining the first item.
 stats.beginning_item()
 
 clear()
+
+#Function for spawning random food and adding food to the player's inventory.
 foods = food.randomFood()
 hotkeys.inventory.append(foods)
 
+#Start of the game loop.
 Quest = input("You take an item into your inventory. The elder leads you to the entrance of the dungeon.\n"
 "The end of this story lies entirely in your hands. Are you ready? (Yes, No): ").lower()
 
 if Quest == "no":
-
     print("\nEveryone got scared and left, and you stayed alone :(")
 
 elif Quest == "yes":
-
     while True:
 
         clear()
+
+        #Function for creating a random room.
         Rsize, Rtype = Locations.locationGen()
+
+        #Property: while the Event variable is greater than or equal to zero, the loop continues.
         if Events.event >= 0:
             Events.quest1(rooms, Rtype)
 
-            if Events.event >= rooms:
-                print("You finished the game!")
-                input("Press any key to finish the game...")
-                break
+        #Property: as soon as the variable reaches the selected difficulty level, the game loop stops.
+        if Events.event >= rooms:
+            print("You finished the game!")
+            input("Press any key to finish the game...")
+            break
             
     else:
 
